31bc2c771bf44c31ffdc3c7b3d48596cf58f65c8
shivam-kumar-07 <shivamkumar.sk7800@gmail.com>
2025-11-12 15:43:14 +0530

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,13 @@ import { Metadata } from 'next';
 import ClientLayout from './layout.client';
 
 export const metadata: Metadata = {
-  description: 'DMM Portals Application',
+  description: 'Fleet Tracking App',
   // icons: {
   //   apple: S3_BASE_URL + '/icons/dmm-logo.svg',
   //   icon: S3_BASE_URL + '/icons/dmm-logo.svg',
   //   shortcut: S3_BASE_URL + '/icons/dmm-logo.svg',
   // },
-  title: 'DMM Admin Portal',
+  title: 'Fleet Tracking APP',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
